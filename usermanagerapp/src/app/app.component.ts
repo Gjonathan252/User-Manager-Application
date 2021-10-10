ce23300d65d99f90ebaa9d2486af6185d3a1c3b9
Gjonathan252 <g.jonathan252@gmail.com>
2021-10-09 19:40:19 -0700

    first commit

---
+++ usermanagerapp/src/app/app.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import { UsermanagerService } from './usermanager.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public users: User[] = [];
+  public editUser!: User;
+  public deleteUser!: User;
+
+  constructor(private usermanagerservice: UsermanagerService){}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  public getUsers(): void {
+    this.usermanagerservice.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+        console.log(this.users);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddUser(addForm: NgForm): void {
+    document.getElementById('add-user-form')?.click();
+    this.usermanagerservice.addUsers(addForm.value).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateUser(user: User): void {
+    this.usermanagerservice.updateUsers(user).subscribe(
+      (response: User) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteUser(userId: number): void {
+    this.usermanagerservice.deleteUsers(userId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getUsers();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchUsers(key: string): void {
+    console.log(key);
+    const results: User[] = [];
+    for (const user of this.users) {
+      if (user.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || user.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || user.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || user.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(user);
+      }
+    }
+    this.users = results;
+    if (results.length === 0 || !key) {
+      this.getUsers();
+    }
+  }
+
+  public onOpenModal(user: User, mode: string): void {
+    const container = document.getElementById('main-container')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addUserModal');
+    }
+    if (mode === 'edit') {
+      this.editUser = user;
+      button.setAttribute('data-target', '#updateUserModal');
+    }
+    if (mode === 'delete') {
+      this.deleteUser = user;
+      button.setAttribute('data-target', '#deleteUserModal');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+
+
+}
